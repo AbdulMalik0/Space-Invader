75b124c07c429a6c2d560de49d5f8a9567e4e5d0
Malik Adebayo <MalikAdebayo@users.noreply.github.com>
2020-05-08 00:43:10 +0100

    Add project files.

---
+++ space_mission_game/Form1.Designer.cs
@@ -0,0 +1,114 @@
+﻿namespace space_mission_game
+{
+    partial class main_Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.stars_timer1 = new System.Windows.Forms.Timer(this.components);
+            this.actor_gunner = new System.Windows.Forms.PictureBox();
+            this.go_left_timer1 = new System.Windows.Forms.Timer(this.components);
+            this.go_right_timer2 = new System.Windows.Forms.Timer(this.components);
+            this.go_up_timer3 = new System.Windows.Forms.Timer(this.components);
+            this.go_down_timer4 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.actor_gunner)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // stars_timer1
+            // 
+            this.stars_timer1.Enabled = true;
+            this.stars_timer1.Interval = 10;
+            this.stars_timer1.Tick += new System.EventHandler(this.stars_timer1_Tick);
+            // 
+            // actor_gunner
+            // 
+            this.actor_gunner.BackgroundImage = global::space_mission_game.Properties.Resources.jet;
+            this.actor_gunner.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.actor_gunner.Location = new System.Drawing.Point(302, 451);
+            this.actor_gunner.Name = "actor_gunner";
+            this.actor_gunner.Size = new System.Drawing.Size(50, 50);
+            this.actor_gunner.TabIndex = 0;
+            this.actor_gunner.TabStop = false;
+            // 
+            // go_left_timer1
+            // 
+            this.go_left_timer1.Enabled = true;
+            this.go_left_timer1.Interval = 5;
+            this.go_left_timer1.Tick += new System.EventHandler(this.go_left_timer1_Tick);
+            // 
+            // go_right_timer2
+            // 
+            this.go_right_timer2.Enabled = true;
+            this.go_right_timer2.Interval = 5;
+            this.go_right_timer2.Tick += new System.EventHandler(this.go_right_timer2_Tick);
+            // 
+            // go_up_timer3
+            // 
+            this.go_up_timer3.Enabled = true;
+            this.go_up_timer3.Interval = 5;
+            this.go_up_timer3.Tick += new System.EventHandler(this.go_up_timer3_Tick);
+            // 
+            // go_down_timer4
+            // 
+            this.go_down_timer4.Enabled = true;
+            this.go_down_timer4.Interval = 5;
+            this.go_down_timer4.Tick += new System.EventHandler(this.go_down_timer4_Tick);
+            // 
+            // main_Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Desktop;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(684, 511);
+            this.Controls.Add(this.actor_gunner);
+            this.KeyPreview = true;
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(700, 550);
+            this.MinimizeBox = false;
+            this.Name = "main_Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Space Mission";
+            this.Load += new System.EventHandler(this.main_Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.main_Form1_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.main_Form1_KeyUp);
+            ((System.ComponentModel.ISupportInitialize)(this.actor_gunner)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer stars_timer1;
+        private System.Windows.Forms.PictureBox actor_gunner;
+        private System.Windows.Forms.Timer go_left_timer1;
+        private System.Windows.Forms.Timer go_right_timer2;
+        private System.Windows.Forms.Timer go_up_timer3;
+        private System.Windows.Forms.Timer go_down_timer4;
+    }
+}
+
---
+++ space_mission_game/Form1.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace space_mission_game
+{
+    public partial class main_Form1 : Form
+    {
+        public main_Form1()
+        {
+            InitializeComponent();
+        }
+        void testing_location()
+        {
+
+        }
+
+        int background_speed = 6;
+        PictureBox [] bckgnd_stars;
+        Random stars_pst;
+
+        private void main_Form1_Load(object sender, EventArgs e)
+        {
+            bckgnd_stars = new PictureBox[16];
+            stars_pst = new Random();
+
+            for (int i = 0; i < bckgnd_stars.Length; i++)
+            {
+                bckgnd_stars[i] = new PictureBox();
+                bckgnd_stars[i].BorderStyle = BorderStyle.None;
+                bckgnd_stars[i].Location = new Point(stars_pst.Next(10, 690), stars_pst.Next(-10, 450));
+                if (i % 2 == 1)
+                {
+                    bckgnd_stars[i].Size = new Size(2, 2);
+                    bckgnd_stars[i].BackColor = Color.Silver;
+                }
+                else
+                {
+                    bckgnd_stars[i].Size = new Size(3, 3);
+                    bckgnd_stars[i].BackColor = Color.Moccasin;
+                }
+            this.Controls.Add(bckgnd_stars[i]);
+            }
+        }
+        private void stars_timer1_Tick(object sender, EventArgs e)
+        {
+            for (int i = 0; i < bckgnd_stars.Length / 2; i++)
+            {
+                bckgnd_stars[i].Top += background_speed;
+                if (bckgnd_stars[i].Top >= this.Height)
+                {
+                    bckgnd_stars[i].Top = -bckgnd_stars[i].Height;
+                }
+            }
+            for (int i = bckgnd_stars.Length / 2; i < bckgnd_stars.Length; i++)
+            {
+                bckgnd_stars[i].Top += background_speed - 2;
+                if (bckgnd_stars[i].Top >= this.Height)
+                {
+                    bckgnd_stars[i].Top = -bckgnd_stars[i].Height;
+                }
+            }
+        }
+
+        private void go_left_timer1_Tick(object sender, EventArgs e)
+        {
+            if (actor_gunner.Left >= 5)
+            {
+                actor_gunner.Left -= background_speed;
+            }
+        }
+
+        private void go_right_timer2_Tick(object sender, EventArgs e)
+        {
+            if (actor_gunner.Left <= 495) 
+            {
+                actor_gunner.Left += background_speed;
+            }
+        }
+
+        private void go_up_timer3_Tick(object sender, EventArgs e)
+        {
+            if(actor_gunner.Top > 5)
+            {
+                actor_gunner.Top -= background_speed;
+            }
+        }
+
+        private void go_down_timer4_Tick(object sender, EventArgs e)
+        {
+            if(actor_gunner.Top <= 695)
+            {
+                actor_gunner.Top += background_speed;
+            }
+        }
+
+        private void main_Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if(e.KeyCode == Keys.Left)
+            {
+                go_left_timer1.Start();
+            }
+            if(e.KeyCode == Keys.Right)
+            {
+                go_right_timer2.Start();
+            }
+
+        }
+
+        private void main_Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+
+        }
+    }
+}
